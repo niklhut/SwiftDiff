b0d6c994f490b835de41862c6eb6511c2ffffff0
niklhut <49069026+niklhut@users.noreply.github.com>
2022-07-07 11:37:19 +0200

    Improvements for linux

--- Sources/SwiftDiff/NSRegularExpression.swift
+++ Sources/SwiftDiff/NSRegularExpression.swift
@@ -6,7 +6,7 @@ extension NSRegularExpression {
     // NOTE: swiftc crashes when using a typealias for matching options
 
     #if os(Linux)
-    func matches(_ string: String, options: NSMatchingOptions = []) -> Bool {
+    func matches(_ string: String, options: NSRegularExpression.MatchingOptions = []) -> Bool {
         let range = NSRange(location: 0, length: string.unicodeScalars.count)
         return firstMatch(in: string, options: options, range: range) != nil
     }
